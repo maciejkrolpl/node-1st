78c3770b67fb44f488a3f0db4dc1416dcc98aac2
Maciej Król <external.maciej.krol@hannover-re.com>
2021-02-14 13:09:36 +0100

    Postgresql added

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const cool = require('cool-ascii-faces');
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+const {Pool} = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -11,6 +18,18 @@ express()
   .get('/cool', (req, res) => res.send(cool()))
   .get('/stream', (req, res) => res.send(cool.faceStream()))
   .get('/times', (req, res) => res.send(showTimes()))
+  .get('/db', async (req, res)=> {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results' : (result) ? result.rows : null};
+      res.render('pages/db', results);
+      client.release();
+    } catch(error) {
+      console.error(error);
+      res.send('Error ' + error);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
   
 showTimes = () => {
